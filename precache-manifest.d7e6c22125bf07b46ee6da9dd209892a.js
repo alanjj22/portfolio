46ef6da0e9cb0912e2212f604bfd249fc77e9515
alanjj22 <alanjomyjosephstm@gmail.com>
2020-08-24 19:48:21 +0530

    Updates

---
+++ precache-manifest.d7e6c22125bf07b46ee6da9dd209892a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21c14511770470f3928dc169f9689f9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fae558621835dc1a6c09",
+    "url": "/portfolio/static/css/main.a15b6a38.chunk.css"
+  },
+  {
+    "revision": "55f4cf7adb6c14501d5a",
+    "url": "/portfolio/static/js/2.fb0d2001.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.fb0d2001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae558621835dc1a6c09",
+    "url": "/portfolio/static/js/main.711d6fe2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0139753803c407404180c45e4b1d2655",
+    "url": "/portfolio/static/media/profile.01397538.JPG"
+  }
+]);
